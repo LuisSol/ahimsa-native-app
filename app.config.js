1bf48d872b244f61a20a3579fddab65f96605ec8
Luis Enrique Solis Navarro <luisenrique.solisnavarro@gmail.com>
2020-06-24 20:30:27 -0600

    Added configs to deploy to App store

--- app.config.js
+++ app.config.js
@@ -12,10 +12,20 @@ export default {
   slug: "lesn-ahimsa",
   ios: {
     bundleIdentifier: "com.lesn.ahimsa",
-    buildNumber: "1.0.0"
+    buildNumber: "1.0.0",
+    infoPlist: {
+      NSCalendarsUsageDescription: "Esta app NO usa la cámara del equipo, sin embargo esta desarrollada con Expo Kit.",
+      NSContactsUsageDescription: "Esta app NO usa la lista de usarios del equipo, sin embargo esta desarrollada con Expo Kit.",
+      NSLocationWhenInUseUsageDescription: "Esta app NO usa la localización del equipo, sin embargo esta desarrollada con Expo Kit.",
+      NSMicrophoneUsageDescription: "Esta app NO usa el micrófono del equipo, sin embargo esta desarrollada con Expo Kit.",
+      NSMotionUsageDescription: "Esta app NO usa el acelerometro del equipo, sin embargo esta desarrollada con Expo Kit.",
+      NSPhotoLibraryAddUsageDescription: "Esta app NO guarda imágenes en el equipo, sin embargo esta desarrollada con Expo Kit.",
+      NSCameraUsageDescription: "Esta app NO usa la cámara del equipo, sin embargo esta desarrollada con Expo Kit."
+    },
   },
   android: {
     package: "com.lesn.ahimsa",
-    versionCode: 1
+    versionCode: 2,
+    permissions: []
   }
 };
